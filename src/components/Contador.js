3bb8134c3ba459de934a539f2d42c682cdb6b6f9
KiriShiroku <paquito24goros@gmail.com>
2024-01-19 21:58:37 -0600

    commit

---
+++ src/components/Contador.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export function Contador() {
+  const [cont, setCont] = useState(0);
+  function avanzar() {
+    setCont(cont + 1);
+  }
+
+  return (
+    <>
+      <div>El contador es: {cont}</div>
+      <button onClick={avanzar}>Incrementa el contador</button>
+    </>
+  );
+}
